90f933346a83be31f7a38dbc537a6f9960d90c0a
Thomas Lennon <thomas.lennon@gmail.com>
2019-12-05 09:26:27 +0000

    added failing test cases for the computer

--- 2019/src/main/java/ie/tomlennon/aoc/day2/IntcodeComputer.java
+++ 2019/src/main/java/ie/tomlennon/aoc/day2/IntcodeComputer.java
@@ -1,7 +1,13 @@
 package ie.tomlennon.aoc.day2;
 
+import java.util.List;
+
 public class IntcodeComputer {
     public static final int ADDITION_OPT_CODE = 1;
     public static final int MULTIPLICATION_OPT_CODE = 2;
+
+    public int calculate(List<Integer> code){
+        return 0;
+    }
 
 }
--- 2019/src/test/groovy/IntCodeSpec.groovy
+++ 2019/src/test/groovy/IntCodeSpec.groovy
@@ -3,16 +3,16 @@ import ie.tomlennon.aoc.day2.IntcodeComputer
 class IntCodeSpec extends spock.lang.Specification {
     def "Calculate"() {
         expect:
-        new IntcodeComputer().calculateFuel(masses) == result
+        new IntcodeComputer().calculate(codes) == result
 
         where:
-        masses      || result
-        [12]        || 2
-        [14]        || 2
-        [12, 14]     | 4
-        [12, 14, 12] | 6
-        [1969]      || 654
-        [100756]    || 33583
+        codes      || result
+        [1,9,10,3,2,3,11,0,99,30,40,50]        || [3500,9,10,70,2,3,11,0,99,30,40,50]
+        [1,0,0,0,99] || [2,0,0,0,99] //(1 + 1 = 2)
+        [2,3,0,3,99] || [2,3,0,3,99] //(3 * 2 = 6)
+        [2,4,4,5,99,0] || [2,4,4,5,99,9801] //(99 * 99 = 9801).
+        [1,1,1,4,99,5,6,0,99] || [30,1,1,4,2,5,6,0,99]
+
     }
 
 }
